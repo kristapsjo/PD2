81e5e8241aeb2bd08b68ed2a9c19a735af414ab4
KJ <kristapsjosts@gmail.com>
2022-10-19 07:42:46 +0000

    samainita api atslega

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "zxa1pdZJLVDu6Kv8Aj0QNxcesKODjc8GfwtPmxe5"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
